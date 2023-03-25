32b5caf9563fafba439647f392ca37641dc3e3b5
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-03-25 16:20:22 +0900

    [Gold V] Title: 리모컨, Time: 376 ms, Memory: 68520 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws  IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        int m = Integer.parseInt(st.nextToken());
+
+        boolean[] broke = new boolean[10];
+
+        if(m != 0){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int i = 0; i<m; i++){
+                int a = Integer.parseInt(st.nextToken());
+                broke[a] = true;
+            }
+        }
+
+        int target = Math.abs(n - 100);
+
+        if(target != 0){
+            for(int i = 0; i<=999999; i++){
+                String str = String.valueOf(i);
+
+                boolean pressed = false;
+                for(int j = 0; j<str.length(); j++){
+                    if(broke[str.charAt(j) - '0']){
+                        pressed = true;
+                        break;
+                    }
+                }
+                if(!pressed){
+                    int gap = Math.abs(n - i);
+                    target = Math.min(target, (gap + str.length()));
+                }
+            }
+        }
+
+        System.out.print(target);
+
+
+    }
+}
