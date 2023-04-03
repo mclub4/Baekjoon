6469f004e2c6b240fae9965106d12c25b0ff5171
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-04-03 13:12:35 +0900

    [Gold III] Title: 줄 세우기, Time: 532 ms, Memory: 45600 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] input;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+
+        int n,m;
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer>[] nodelist = new ArrayList[n+1];
+        input = new int[n+1];
+
+        for(int i = 1; i<n+1; i++){
+            nodelist[i] = new ArrayList<>();
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            nodelist[start].add(end);
+            input[end] ++;
+        }
+
+        for(int i = 1; i<n+1; i++){
+            if(input[i] == 0) queue.add(i);
+        }
+
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+//            int tmp_next = nodelist[tmp].size();
+            sb.append(tmp + " ");
+            for(int i = 0; i<nodelist[tmp].size(); i++){
+                int next = nodelist[tmp].get(i);
+                input[next] --;
+                if(input[next] == 0){
+                    queue.add(next);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
