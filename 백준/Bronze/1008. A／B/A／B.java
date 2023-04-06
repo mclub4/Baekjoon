61493b6484402405f3532aa43296408f91c0ccd2
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-04-06 20:02:58 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 18028 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double n = sc.nextDouble();
+        double m = sc.nextDouble();
+
+        double result = n/m;
+        System.out.println(result);
+    }
+
+}
