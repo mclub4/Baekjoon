938bd0132d6a0fc49ce15d6eb479fab5a1be16c1
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-04-24 20:05:05 +0900

    [Silver III] Title: N과 M (1), Time: 324 ms, Memory: 34520 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n,m;
+    static int[] result;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n];
+        result = new int[m];
+        nqueen(0);
+        System.out.println(sb);
+    }
+
+    public static void nqueen(int depth){
+        if(depth == m){
+            for(int i = 0; i<result.length; i++){
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i<n; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                result[depth] = i+1;
+                nqueen(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
