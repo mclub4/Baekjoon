9d1ff150d629d6614d275c199bb84c421078bdc9
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-04-27 14:34:26 +0900

    [Gold III] Title: 학교 탐방하기, Time: 820 ms, Memory: 155316 KB -BaekjoonHub

---
+++ 백준/Gold/13418. 학교 탐방하기/학교 탐방하기.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Node implements Comparable<Node>{
+        int start;
+        int end;
+        int cost;
+
+        public Node(int start, int end, int cost){
+            this.start = start;
+            this.end = end;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o){
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+
+    static int[] parent;
+    static ArrayList<Node> nodeList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        parent = new int [v+1];
+        nodeList = new ArrayList<>(e+1);
+
+
+        for(int i = 0; i<v+1; i++){
+            parent[i] = i;
+        }
+
+        for(int i = 0; i<e+1; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            Node tmp = new Node(start, end, cost);
+            nodeList.add(tmp);
+        }
+
+        Collections.sort(nodeList);
+
+        int answer = 0;
+
+        for(int i = 0; i<e+1; i++){
+            Node tmp = nodeList.get(i);
+            if(find(tmp.start) != find(tmp.end)){
+                union(tmp.start, tmp.end);
+                if(tmp.cost == 0) answer++;
+            }
+        }
+
+        for(int i = 0; i<v+1; i++){
+            parent[i] = i;
+        }
+
+        int answer2 = 0;
+
+        for(int i = e; i>=0; i--){
+            Node tmp = nodeList.get(i);
+            if(find(tmp.start) != find(tmp.end)){
+                union(tmp.start, tmp.end);
+                if(tmp.cost == 0) answer2++;
+            }
+        }
+
+        System.out.println(answer*answer-answer2*answer2);
+    }
+
+    public static int find(int v){
+        if(parent[v] == v) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    public static void union(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if(parent_a != parent_b){
+            if (parent_a < parent_b) {
+                parent[parent_b] = parent_a;
+            } else {
+                parent[parent_a] = parent_b;
+            }
+        }
+    }
+
+}
