017a653c7e781cad721eef31139fcf6e154214e7
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-04-27 12:19:28 +0900

    [Platinum V] Title: 선분 그룹, Time: 488 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Platinum/2162. 선분 그룹/선분 그룹.java
+++ 백준/Platinum/2162. 선분 그룹/선분 그룹.java
@@ -36,30 +36,9 @@ public class Main {
             long y2 = Long.parseLong(st.nextToken());
             nodelist[i] = new Line(x1, y1, x2, y2);
         }
-
-//        for(int i = 1; i<n+1; i++){
-//            for(int j = 1; j<n+1; j++){
-//                if(cross(nodelist[i], nodelist[j])){
-//                    if(find(i) != find(j)){
-//                        union(i, j);
-//                    }
-//                }
-//            }
-//        }
-
-//        for(int i = 1; i<n; i++){
-//            for(int j = i+1; j<n+1; j++){
-//                if(cross(nodelist[i], nodelist[j])){
-//                    if(find(i) != find(j)){
-//                        union(i, j);
-//                    }
-//                }
-//            }
-//        }
-
+        
         for(int i = 1; i<n+1; i++){
             for(int j = 1; j<n+1; j++){
-                if(i==j) continue;
                 if(find(i) != find(j)){
                     if(cross(nodelist[i], nodelist[j])){
                         union(i,j);
@@ -73,7 +52,6 @@ public class Main {
         int size = 0;
         for(int i = 1; i<n+1; i++){
             cnt[parent[i]]++;
-//            System.out.println(parent[i] + "가 대상임");
         }
         for(int i = 1; i<n+1; i++){
             if(max < cnt[i]) max = cnt[i];
@@ -89,19 +67,7 @@ public class Main {
         long a = ccw(line1.x1, line1.y1, line1.x2, line1.y2, line2.x1, line2.y1) * ccw(line1.x1, line1.y1, line1.x2, line1.y2, line2.x2, line2.y2);
         //b<0 또는 b = 0
         long b = ccw(line2.x1, line2.y1, line2.x2, line2.y2, line1.x1, line1.y1) * ccw(line2.x1, line2.y1, line2.x2, line2.y2, line1.x2, line1.y2);
-//        System.out.println("(" + line1.x1 + ", " + line1.y1 + ") 과 " + "(" + line1.x2 + ", " + line1.y2 + ")으로 이루어진 선분과");
-//        System.out.println("(" + line2.x1 + ", " + line2.y1 + ") 과 " + "(" + line2.x2 + ", " + line2.y2 + ")으로 이루어진 선분은");
-//        System.out.println(a + "와 " + b + "가 나왔습니다.");
-        if(a == 0 && b == 0){
-            boolean test = overlap(line1, line2);
-//            System.out.println("따라서 " + test + "입니다.");
-            return test;
-        }
-//        if(a<=0 && b<=0){
-////            System.out.println("따라서 " + true + "입니다.");
-//            return true;
-//        }
-//        System.out.println("따라서 " + false + "입니다.");
+        if(a == 0 && b == 0)return overlap(line1, line2);
         if(a<=0 && b<=0) return true;
         return false;
     }
@@ -116,7 +82,6 @@ public class Main {
 
     public static int ccw(long x1, long y1, long x2, long y2, long x3, long y3){
         long result = (x1*y2+x2*y3+x3*y1) - (x2*y1+x3*y2+x1*y3);
-//        System.out.println("ccw값은 " + result);
         if(result>0) return -1;
         else if(result == 0) return 0;
         else return 1;
@@ -130,8 +95,7 @@ public class Main {
     public static void union(int a, int b){
         int parent_a = find(a);
         int parent_b = find(b);
-//        if(parent_a == 3 || parent_b ==3) System.out.println("지금!");
-
+        
         if(parent_a != parent_b){
             if (parent_a > parent_b) {
                 parent[parent_a] = parent_b;
@@ -140,14 +104,4 @@ public class Main {
             }
         }
     }
-//    public static void union(int x, int y) {
-//        x = find(x);
-//        y = find(y);
-//
-//        if (x != y) {
-//            parent[x] = y;
-//        } else {
-//            return;
-//        }
-//    }
 }
