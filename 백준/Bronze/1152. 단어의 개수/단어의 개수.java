91d41a4b288e3a8cf887149061c26749bbdb98d6
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-05-01 11:22:25 +0900

    [Bronze II] Title: 단어의 개수, Time: 280 ms, Memory: 26476 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        a = a.trim();
+        if(!a.equals("")){
+            String answer[] = a.split(" ");
+            System.out.println(answer.length);
+        }
+        else System.out.println(0);
+    }
+}
