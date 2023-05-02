2f11971b4b20a460a7195a2bd303e99c1d962816
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-05-02 14:14:56 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 548 ms, Memory: 66960 KB -BaekjoonHub

---
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static char[][] board;
+    static int[] parent;
+    static boolean[] visited;
+    static int n,m;
+    static int[] dir_x = {1,-1,0,0};
+    static int[] dir_y = {0,0,1,-1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        board = new char[n][m];
+        parent = new int[n*m];
+        visited = new boolean[n*m];
+
+        for(int i = 0; i<parent.length; i++){
+            parent[i] = i;
+        }
+
+        for(int i =0; i<n; i++){
+            String tmp = br.readLine();
+            for(int j=0; j<m; j++){
+                board[i][j] = tmp.charAt(j);
+            }
+        }
+
+        int answer = 0;
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                int curidx = getidx(i,j);
+                if(!visited[curidx]){
+                    int nextidx;
+                    if(board[i][j] == 'D') nextidx = getidx(i+1, j);
+                    else if(board[i][j] == 'L') nextidx = getidx(i, j-1);
+                    else if(board[i][j] == 'R') nextidx = getidx(i, j+1);
+                    else nextidx = getidx(i-1, j);
+                    if(find(curidx) != find(nextidx)){
+                        union(curidx, nextidx);
+                    }
+                    visited[curidx] = true;
+                }
+            }
+        }
+
+//        int[] cnt = new int[n*m];
+//
+//        for(int i = 0; i<cnt.length; i++){
+//            cnt[parent[i]]++;
+//        }
+//
+//        for(int i = 0; i<cnt.length; i++){
+//            if(cnt[i] != 0) answer ++;
+//        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<parent.length; i++){
+            set.add(find(i));
+        }
+
+        System.out.println(set.size());
+    }
+
+    public static int getidx(int i, int j){
+        return i*m + j;
+    }
+
+    public static int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public static void union(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if(parent_a != parent_b){
+            if(parent_a<parent_b) parent[parent_b] = parent_a;
+            else parent[parent_a] = parent_b;
+        }
+    }
+}
