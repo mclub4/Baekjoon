ea377bed56953ff2fac7af9519fb6bf6aa58886b
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-05-18 01:56:02 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 300 ms, Memory: 24628 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[] building = new long[n];
+
+        for(int i = 0; i<n; i++){
+            building[i] = Long.parseLong(br.readLine());
+        }
+
+        Stack<Long> stack = new Stack<>();
+//        stack.push(building[0]);
+
+        long result = 0;
+
+        for(int i = 0; i<n; i++){
+            while(!stack.isEmpty() && stack.peek() <= building[i]) stack.pop();
+            stack.push(building[i]);
+            result += stack.size() - 1;
+        }
+
+        System.out.println(result);
+    }
+}
