4065588cc5128c6bd9804be38e5a35e6bcb2fbec
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-05-26 14:14:57 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 548 ms, Memory: 125592 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n+1];
+        for(int i = 1; i<n+1; i++){
+            parent[i] = i;
+        }
+
+        int[][] query = new int[m][2];
+
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            query[i][0] = a;
+            query[i][1] = b;
+        }
+
+        for(int i = 0; i<m; i++){
+            union(query[i][0], query[i][1]);
+        }
+        for(int i = 0; i<m; i++){
+            union(query[i][0], query[i][1]);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 1; i<n+1; i++){
+            set.add(parent[i]);
+        }
+
+        System.out.println(set.size());
+    }
+
+    public static int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public static void union(int a, int b){
+        int parent_a = find(a);
+        int parent_b = find(b);
+
+        if(parent_a != parent_b){
+            if(parent_a<parent_b) parent[parent_b] = parent_a;
+            else parent[parent_a] = parent_b;
+        }
+    }
+}
