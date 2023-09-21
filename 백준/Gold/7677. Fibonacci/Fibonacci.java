ad4fed9ed9e1880d45d3ea002e2f81c276a8f033
mclub4 <55117706+mclub4@users.noreply.github.com>
2023-09-21 14:23:20 +0900

    [Gold II] Title: Fibonacci, Time: 128 ms, Memory: 14748 KB -BaekjoonHub

---
+++ 백준/Gold/7677. Fibonacci/Fibonacci.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static long[][] standard = {{1,1}, {1,0}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            long n = Long.parseLong(br.readLine());
+            if(n == -1) break;
+            if(n==0) System.out.println(0);
+            else System.out.println(fibo(standard, n-1)[0][0]);
+        }
+    }
+
+    public static long[][] fibo(long[][] arr, long n){
+        if(n == 0 || n==1) return arr;
+
+        long[][] ret = fibo(arr, n/2);
+
+        ret = multiply(ret, ret);
+
+        if(n%2 != 0){
+            ret = multiply(ret, standard);
+        }
+
+        return ret;
+    }
+
+    public static long[][] multiply(long[][] arr, long[][] origin){
+        long[][] answer = new long[2][2];
+
+        answer[0][0] = (arr[0][0]*origin[0][0] + arr[0][1]*origin[1][0])%10000;
+        answer[0][1] = (arr[0][0]*origin[0][1] + arr[0][1]*origin[1][1])%10000;
+        answer[1][0] = (arr[1][0]*origin[0][0] + arr[1][1]*origin[1][0])%10000;
+        answer[1][1] = (arr[1][0]*origin[0][1] + arr[1][1]*origin[1][1])%10000;
+
+        return answer;
+    }
+}
