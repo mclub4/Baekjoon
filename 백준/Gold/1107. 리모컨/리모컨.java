5793101a0d101b15c4d6f594b61f0410a95cadbe
HYUNJIN CHO <55117706+mclub4@users.noreply.github.com>
2023-11-04 11:18:37 +0900

    [Gold V] Title: 리모컨, Time: 296 ms, Memory: 64672 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -4,48 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws  IOException {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(br.readLine());
 
-        int m = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(br.readLine());
+        
 
-        boolean[] broke = new boolean[10];
+        boolean[] broken = new boolean[10];
 
-        if(m != 0){
-            st = new StringTokenizer(br.readLine(), " ");
+        if(m!=0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int i = 0; i<m; i++){
-                int a = Integer.parseInt(st.nextToken());
-                broke[a] = true;
+                int tmp = Integer.parseInt(st.nextToken());
+                broken[tmp] = true;
             }
         }
 
         int target = Math.abs(n - 100);
 
         if(target != 0){
-            for(int i = 0; i<=999899; i++){
+            for(int i = 0; i<=999999; i++){
                 String str = String.valueOf(i);
 
                 boolean pressed = false;
+
                 for(int j = 0; j<str.length(); j++){
-                    if(broke[str.charAt(j) - '0']){
+                    if(broken[str.charAt(j) - '0']){
                         pressed = true;
                         break;
                     }
                 }
+
                 if(!pressed){
-                    int gap = Math.abs(n - i);
+                    int gap = Math.abs(n-i); //+ - 누르는 횟수
                     target = Math.min(target, (gap + str.length()));
                 }
             }
         }
 
-        System.out.print(target);
-
-
+        System.out.println(target);
     }
 }
