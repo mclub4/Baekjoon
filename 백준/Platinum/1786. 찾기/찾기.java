25dbcbcd4533e4afb41f598c0b1448701f4a87f9
HYUNJIN CHO <55117706+mclub4@users.noreply.github.com>
2023-11-28 09:32:18 +0900

    [Platinum V] Title: 찾기, Time: 704 ms, Memory: 87188 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,55 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
-    static int[] suffix;
-    static ArrayList<Integer> list;
-
+    static String T,P;
+    static int[] pi;
+    static ArrayList<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String T = br.readLine();
-        String P = br.readLine();
-        suffix = new int[P.length()+1];
-        list = new ArrayList<>();
-        maketable(P);
-        KMP(T,P);
+        T = br.readLine();
+        P = br.readLine();
 
+        pi = new int[P.length()];
+        maketable();
+        KMP();
 
-        System.out.println(list.size());
-        for(int i : list){
-            System.out.print(i + " ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(list.size() + "\n");
+        for(int i = 0; i<list.size(); i++){
+            sb.append(list.get(i) + " ");
         }
+        System.out.println(sb);
     }
 
-    public static void maketable(String P){
+    public static void maketable(){
         int j = 0;
         for(int i = 1; i<P.length(); i++){
-            while(P.charAt(j) != P.charAt(i) && j>0){
-                j = suffix[j-1];
+            while(j>0 && P.charAt(j) != P.charAt(i)){
+                j = pi[j-1];
             }
-            if(P.charAt(j) == P.charAt(i)){
-                suffix[i] = ++j;
+            if(P.charAt(i) == P.charAt(j)){
+                pi[i] = ++j;
             }
         }
     }
 
-    public static void KMP(String T, String P){
-        int n = T.length();
-        int m = P.length();
+    public static void KMP(){
         int j = 0;
-        for(int i = 0; i<n; i++){
-            while(j>0 && T.charAt(i) != P.charAt(j)) j = suffix[j-1];
-            if(T.charAt(i) == P.charAt(j)){
-                if(j == m-1){
-                    list.add(i - m + 2);
-                    j = suffix[j];
+        for(int i = 0; i<T.length(); i++){
+            while(j>0 && P.charAt(j) != T.charAt(i)){
+                j = pi[j-1];
+            }
+            if(P.charAt(j) == T.charAt(i)){
+                if(j == P.length()-1){
+                    list.add(i - P.length() + 2);
+                    j = pi[j];
                 }
                 else j++;
             }
         }
     }
-
-
 }
