4538f9f167a71d7a9dea0ae56c333801f11b4b3f
HYUNJIN CHO <55117706+mclub4@users.noreply.github.com>
2023-12-01 08:51:36 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 156 ms, Memory: 14652 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        int max = 1;
+
+        for(int i = 0; i<n; i++){
+            dp[i] = 1;
+            for(int j = 0; j<i; j++){
+                if(arr[i] > arr[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(max);
+        Stack<Integer> stack = new Stack<>();
+        for(int i = n-1; i>=0; i--){
+            if(dp[i] == max){
+                stack.add(arr[i]);
+                max --;
+            }
+            if(max == 0) break;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop() + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
