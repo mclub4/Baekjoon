c46b6b899ac2f97317a010094b0f0b9297fdf0d6
HYUNJIN CHO <55117706+mclub4@users.noreply.github.com>
2023-12-04 22:18:56 +0900

    [Gold II] Title: 미확인 도착지, Time: 516 ms, Memory: 59640 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.java
@@ -1,104 +1,105 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    public static class Node implements Comparable<Node> {
+        int end, weight;
 
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int INF = 100_000_000;
-    private static int T;
-    private static int V, E, t;
-    private static List<List<Node>> list;
-    private static int dist[];
-    private static List<Integer> candidate;
-
-    static class Node implements Comparable<Node> {
-
-        int v, weight;
-
-        public Node(int v, int weight) {
-            this.v = v;
+        public Node(int end, int weight) {
+            this.end = end;
             this.weight = weight;
         }
 
         @Override
         public int compareTo(Node o) {
-            return weight - o.weight;
+            return this.weight - o.weight;
         }
     }
 
+    static ArrayList<Node>[] nodelist;
+    static int[] dist;
+    static int n;
+
     public static void main(String[] args) throws IOException {
-        T = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int test = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < T; i++) {
+        while(test-->0){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
-            t = Integer.parseInt(st.nextToken());
 
-            dist = new int[V + 1];
-            Arrays.fill(dist, INF);
-
-            list = new ArrayList<>();
-            for (int j = 0; j < V + 1; j++)
-                list.add(new ArrayList<>());
+            n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int t = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
             int g = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
 
-            for (int j = 0; j < E; j++) {
-                st = new StringTokenizer(br.readLine());
-                int u = Integer.parseInt(st.nextToken());
-                int v = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());
+            nodelist = new ArrayList[n+1];
+            for(int i = 1; i<n+1; i++){
+                nodelist[i] = new ArrayList<>();
+            }
 
-                if ((u == g && v == h) || (u == h && v == g)) {
-                    list.get(u).add(new Node(v, weight * 2 - 1));
-                    list.get(v).add(new Node(u, weight * 2 - 1));
+            for(int i = 0; i<m; i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                int d = Integer.parseInt(st.nextToken());
+                if ((a == g && b == h) || (a == h && b == g)) {
+                    nodelist[a].add(new Node(b, d * 2 - 1));
+                    nodelist[b].add(new Node(a, d * 2 - 1));
                 } else {
-                    list.get(u).add(new Node(v, weight * 2));
-                    list.get(v).add(new Node(u, weight * 2));
+                    nodelist[a].add(new Node(b, d * 2));
+                    nodelist[b].add(new Node(a, d * 2));
                 }
             }
 
-            // 목적지 후보를 저장한다.
-            candidate = new ArrayList<>();
+            dist = new int[n+1];
+            Arrays.fill(dist, 100000000);
+
+            ArrayList<Integer> destination = new ArrayList<>();
             for (int j = 0; j < t; j++)
-                candidate.add(Integer.parseInt(br.readLine()));
+                destination.add(Integer.parseInt(br.readLine()));
 
-            dijkstra(start);
+            dijkstra(s);
 
-            Collections.sort(candidate);
+            Collections.sort(destination);
 
-            for (int num : candidate) {
+            //만약 최종 간선의 최단 경로가 홀수다? 그럼 g-h경로를 포함한 것임 따라서 답
+            for (int num : destination) {
                 if (dist[num] % 2 == 1) sb.append(num + " ");
             }
+
             sb.append("\n");
         }
 
         System.out.println(sb);
     }
 
-    private static void dijkstra(int u) {
-        boolean[] visited = new boolean[V + 1];
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    //그냥 다익스트라 알고리즘
+    public static void dijkstra(int s){
+        boolean[] check = new boolean[n+1];
 
-        dist[u] = 0;
-        pq.add(new Node(u, 0));
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(s, 0));
+        dist[s] = 0;
 
-        while (!pq.isEmpty()) {
-            Node currentNode = pq.poll();
+        while(!queue.isEmpty()){
+            Node cur = queue.poll();
+            int end = cur.end;
 
-            if (visited[currentNode.v]) continue;
-            visited[currentNode.v] = true;
+            if(check[end] == true) continue;
+            check[end] = true;
 
-            for (Node node : list.get(currentNode.v)) {
-                if (!visited[node.v] && dist[node.v] > dist[currentNode.v] + node.weight) {
-                    dist[node.v] = dist[currentNode.v] + node.weight;
-                    pq.add(new Node(node.v, dist[node.v]));
+            for(Node node : nodelist[end]){
+                if(!check[node.end] && dist[node.end] > dist[end] + node.weight){
+                    dist[node.end] = dist[end] + node.weight;
+                    queue.add(new Node(node.end, dist[node.end]));
                 }
             }
         }
